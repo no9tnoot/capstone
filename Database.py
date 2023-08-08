35e331bf49719326b19037187fdb6cff7250eb3f
emollyr <112810634+emollyr@users.noreply.github.com>
2023-08-08 09:57:53 +0200

    Tried to make a sql connection in database class (not working yet)

--- Database.py
+++ Database.py
@@ -4,14 +4,21 @@
 
 from array import *
 
-import mysql.connector 
+import mysql.connector # python3 --version, and then # pip3.8 install mysql-connector-python
+
 
 class Database:
     
     def __init__(self):
+        database = mysql.connector.connect(
+            host="localhost",
+            user="root"
+        )
+        print(database)
         # get the attributes and their types from SQL, as well as the relations
         self.attributes = [['att1','datatype'],['att2','datatype']]
         self.relations = ['relation1', 'relation2']
+    
     
     def numAttributes(self):
         return len(self.attributes)
