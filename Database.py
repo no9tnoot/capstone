84f402d4e19aa88b0409707a114b8f6637229c22
emollyr <112810634+emollyr@users.noreply.github.com>
2023-08-08 15:07:24 +0200

    Attribute and Relation classes added to Database class script. SQL connection working (in database class) and creating relations and attributes successfully.

--- Database.py
+++ Database.py
@@ -6,52 +6,109 @@ from array import *
 
 import mysql.connector # python3 --version, and then # pip3.8 install mysql-connector-python
 
-database = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="mySQL_sew1",
-    database="sakila"
-)
-# myCursor = database.cursor()
+"""
+    Database attribute (column) with instance variables showing column name, 
+    data type, if there are any nulls in the column, and the key (prime attribute
+    or not)
+"""
+class Attribute:
+    
+    def __init__(self, name, dt, null, k):
+        self.name=name
+        self.dataType=dt
+        self.null=null
+        self.key=k
+    
+    def getName(self):
+        return self.name
+        
+    def getDataType(self):
+        return self.dataType
+        
 
-# myCursor.execute("SELECT * FROM CITY")
-# mySQLoutput = myCursor.fetchall()
-
-# for i in mySQLoutput:
-#   print(i)
 
 
+""" 
+    Database relation (table), with instance variables showing the name 
+    of the relation, and an array containing all the attributes stored in 
+    the relation.
+"""
+class Relation:
+    
+    def __init__(self, n):
+        self.name = n
+        self.attributes=[]
+    
+    def addAttribute(self, attribute):
+        self.attributes.append(attribute)
+        
+    def getAttribute(self, i):
+        # check that the attribute number asked for is not out of bounds
+        if (i<len(self.attributes)):
+            return self.attributes[i]
+        else:
+            return 0
+        
+    def getNumAttributes(self):
+        return len(self.attributes)
+
+
+
+
+""" 
+    Database
+"""
 class Database:
     
-    def __init__(self):
+    def __init__(self, host='localhost', user='root', pword='mySQL_sew1', db_name = 'classicmodels2022'):
+        
+        self.relations = []  # initialise empty array of relations
+        self.loadRelations(db_name)
+        
+        
+    """ Get the attributes and their types from SQL, as well as the relations"""
+    def loadRelations(self, db_name):
+        # connect to sql database
         database = mysql.connector.connect(
             host="localhost",
-            user="root"
+            user="root",
+            password="mySQL_sew1",
+            database=db_name
         )
-        print(database)
-        # get the attributes and their types from SQL, as well as the relations
         
-        # pseudo code (wip)
-        for relation in database:
-            array=["name of relation"]
-            for attribute in relation:
-                array.append([attribute,'datatype'])
-            self.relation.append(array)
+        cursor = database.cursor()  # Create a cursor to interact with the database
         
+        cursor.execute("SHOW TABLES;")   # SQL: print the table names             
+        tables = cursor.fetchall()       # get the output table names from SQL
         
-        #self.relations = [['relation1',['att1','datatype']], ['relation2',['att1','datatype']]]
-    
-
-    def numAttributes(self):
-        return len(self.attributes)
+        # create each relation object and store in relations[]
+        for table in tables:
+            
+            r = Relation(table[0])       # Create a relation for each table name
+            self.relations.append(r)     # store created relation in relations[]
+            
+            cursor.execute("SHOW COLUMNS FROM "+r.name+" ;") # get column details for relation r
+            columns = cursor.fetchall()
+            #create each attribute object for relation r
+            for column in columns:
+                r.addAttribute(Attribute(column[0], column[1], column[2], column[3]))
+        
     
     def numRelations(self):
         return len(self.relations)
        
-    def getAttribute(self, i):
-        return self.attributes[i]
     
     def getRelation(self, i):
         return self.relations[i]
     
-    
+
+
+
+
+
+# testing (delete me)
+new = Database()
+
+print(new.numRelations())
+
+print(new.getRelation(0).getAttribute(0).getDataType())
