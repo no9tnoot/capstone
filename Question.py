cfbab5ace3620a6e998431fe25753661767d470d
Peter <brnpet038@myuct.ac.za>
2023-08-14 11:53:24 +0200

    i'm scared

--- Question.py
+++ Question.py
@@ -1,9 +1,10 @@
 # Molly Ryan
 # 7 August 2023
 # Question Class
-
+import mysql.connector
 import Database
 import random
+import mysql.connector
 # val = random.random()
 # print(val)
 
@@ -17,29 +18,12 @@ class Question:
         self.condition = ['where', 'limit', 'order by', '']
         self.db = database
         self.seed = seed
-        self.questionBuilder()
+        EasyQuestion.easyBuilder(self)
 
-    # remove excess characters from datatype
-    # e.g. smallint unsigned -> smallint, binary(4) -> binary
-
-    def simplifyDatatype(self, attrType):
-
-        # if contains a space, split at space
-        if " " in attrType:
-            attrType = attrType.split(" ")
-
-        # if contains a bracket, split at bracket
-        elif "(" in attrType:
-            attrType = attrType.split("(")
-
-        # ensure attrType will come out of this if else statement as an array
-        else:
-            attrType = attrType.split()
-
-        # return simplified datatype
-        return attrType[0]
-
+    """
+    Functionality now handled in Database class - pull from the database.numericRelations array
     # Returns true if the datatype is numeric (not a string/date/time)
+    """
 
     def isNumeric(self, attrType):
 
@@ -52,68 +36,99 @@ class Question:
 
     # Randomly selects a relation from the loaded database
 
-    def setRel(self, attTypeNeeded):
+    def setRel(self, attTypeNeeded, aggOrCondType):
 
         # select relation
         relation = random.randrange(0, newDB.numRelations()-1, 1)
         relation = newDB.getRelation(relation)
 
-        # if the chosen action is a condition, just return the relation
-        if attTypeNeeded == 'cond':
+        # if the chosen relation contains numeric attributes, return it
+        # if the chosen action is a condition, a count, or nothing, just return the relation
+        if relation.numNumeric() or attTypeNeeded == 'cond' or aggOrCondType == 'count(' or aggOrCondType == '':
             return relation
 
-        # if the chosen action is an aggregate fn or operator, first 
-        # ensure relation contains at least one numeric attribute
+        # if the chosen relation is not appropriate, select a new relation
         else:
-            # iterate through attributes, looking for numeric
-            for i in range(relation.getNumAttributes()):
-
-                # remove excess characters from datatype 
-                # eg. smallint unsigned -> smallint, binary(4) -> binary
-                attrType = self.simplifyDatatype(
-                    relation.getAttribute(i).getDataType())
-
-                # if numeric, return relation
-                if self.isNumeric(attrType):
-                    return relation
-
-                # if no numeric attributes in relation, recurse and select a new relation
-                elif i == relation.getNumAttributes()-1:
-                    '''I'm not confident this will work, as I haven't run an example that tested it.
-                       setAttr() has the same logic, however, and it works''' 
-                    relation = self.setRel(attTypeNeeded)
-                    return relation
+            relation = self.setRel(attTypeNeeded, aggOrCondType)
+            return relation
 
     ''' For questions where 2+ attributes are chosen from one relation, must include logic to prevent the
         attribute being selected twice'''
 
-    def setAttr(self, relation, attTypeNeeded):
-        # select random attribute from relation
-        attribute = random.randrange(0, relation.getNumAttributes()-1, 1)
-        attribute = relation.getAttribute(attribute)
+    def setAttr(self, relation, attTypeNeeded, aggOrCondType, attNum):
 
-        '''Must include option for where attribute given is *. This happens:
-            - for conditions
-            - for counts '''
+        # Randomly select attribute from relation
 
-        # remove excess characters from datatype eg. smallint unsigned -> smallint, binary(4) -> binary
-        attrType = self.simplifyDatatype(attribute.getDataType())
 
-        # if numeric, return attribute
-        if self.isNumeric(attrType):
-            return attribute
-
-        # else, recurse until select numeric attribute
+        # if condition or count, include * as an option
+        '''decided to include * in this way as it will allow it to appear with reasonable frequency'''
+        if attTypeNeeded == 'cond' or aggOrCondType == 'count(':
+            # Randomly select attribute from relation -> doesn't have to be numeric
+            attribute = random.randrange(0, relation.getNumAttributes()-1, 1)
+            attribute = relation.getAttribute(attribute)
+            
+            # Choose between attribute and '*' if attNum is 1
+            if attNum == 1:
+                astOrAttr = random.choice(['*', 'attribute'])
+                if astOrAttr == '*':
+                    attribute = Database.Attribute('*', 'varchar(50)' , 'NO', '')
+        
         else:
-            attribute = self.setAttr(relation, attTypeNeeded)
-            return attribute
+            print(aggOrCondType + " -> ensuring numeric")
+            attribute = random.randrange(0, len(relation.numericAttributes)-1, 1)
+            attribute = relation.numericAttributes[attribute]
+        
+        return attribute
 
-    def questionBuilder(self):
+
+
+    def findNumRows(self, relation, attribute):
+
+        database = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mySQL_sew1',
+            database='classicmodels2022'
+        )
+
+        cursor = database.cursor()  # Create a cursor to interact with the database
+        cursor.execute("SELECT count(" + attribute.name + ") FROM " +
+                       relation.name + ";")   # SQL: print the table names
+        numRows = cursor.fetchall()
+        return numRows[0][0]
+
+
+    # Find a value for an attribute that you will set in the where clause
+    # eg. WHERE attribute = 'attributeVal'
+    def selectAttrVal(self, relation, attribute):
+
+        database = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mySQL_sew1',
+            database='classicmodels2022'
+        )
+
+        cursor = database.cursor()  # Create a cursor to interact with the database
+
+        cursor.execute("SELECT " + attribute.name + " FROM " +
+                       relation.name + ";")   # SQL: print the table names
+        values = cursor.fetchall()       # get the output table names from SQL
+
+        reqVal = random.randrange(0, len(values), 1)
+        reqVal = values[reqVal][0]
+        return reqVal
+
+
+class EasyQuestion(Question):
+
+    def __init__(self, database, seed):
+        super().__init__(database, seed)
+
+    def easyBuilder(self):
         # Randomly select either an aggregate fn or condition
         aggOrCond = random.choice(['agg', 'cond'])
-        # select relation from database
-        relation = self.setRel(aggOrCond)
-        # print("Relation: " + relation.name) # testing (delete me)
+        # aggOrCond = 'cond'
 
         # If the random selection is an aggregate fn
         if aggOrCond == 'agg':
@@ -121,70 +136,70 @@ class Question:
             aggType = random.choice(self.aggregateFunctions)
             # print("Aggregate Function: " + aggType) # testing (delete me)
 
+            # select relation from database
+            relation = Question.setRel(self, aggOrCond, aggType)
+            # print("Relation: " + relation) # testing (delete me)
+
             # select attribute from relation
-            attr = self.setAttr(relation, aggOrCond)
+            attr = self.setAttr(relation, aggOrCond, aggType, 1)
             # print("Attribute: " + attr.name) # testing (delete me)
 
-            print("SELECT " + aggType + attr.name + ") FROM " + relation.name) # testing (delete me)
+            if aggType == '':
+                print("SELECT " + aggType + attr.name +
+                      " FROM " + relation.name + ";")
+            else:
+                print("SELECT " + aggType + attr.name +
+                      ") FROM " + relation.name + ";")
 
         # If the random selection is a condition, select a random column and random condition
         elif aggOrCond == 'cond':
             condType = random.choice(self.condition)
-            print("Condition") # testing (delete me)
+            # condType = "where"
+            # print("Condition: " + condType) # testing (delete me)
 
-    #write the english question for the sql query
-    def englishQuestion(sql):
-        #2d array holds slots for each part of the question
-        english = ['','']
+            relation = Question.setRel(self, aggOrCond, condType)
+            # print("Relation: " + relation) # testing (delete me)
 
-        #aggregates and attributes
-        english[0] = ['Show'] + Question.block1(sql[0], sql[1])
-    
-        #relation
-        english[1] = ' in the ' + sql[2] + ' table.'
+            '''Should I overload this function so that it doesn't have to deal with the numeric checks?'''
+            attr_1 = self.setAttr(relation, aggOrCond, condType, 1)
+            # print("Attribute 1: " + attr_1.name) # testing (delete me)
 
-        #test
-        print(Question.englishToString(english))
+            if condType == '':
+                print("SELECT " + condType + attr_1.name +
+                      " FROM " + relation.name + ";")
 
-    def englishToString(english):
-        question = ''
-        for block in english:
-            for string in block:
-                question += string
-        return question
+            elif condType == 'order by':
+                attr_2 = self.setAttr(relation, aggOrCond, condType, 2)
+                # print("Attribute 2: " + attr_2.name) # testing (delete me)
 
-    def block1(aggregates, attributes):
-        #aggregate functions go in the first slot
-        block1 = []
+                orderBy = random.choice(['ASC', 'DESC'])
+                print("SELECT " + attr_1.name + " FROM " + relation.name +
+                      " ORDER BY " + attr_2.name + ' ' + orderBy + ';')
 
-        for x in range(len(attributes)):
-            match aggregates[x]:
-                case 'count':
-                    block1.append(' how many rows there are in ')
-                case 'max':
-                    block1.append(' the greatest value of ')
-                case 'min':
-                    block1.append(' the smallest value of ')
-                case 'avg':
-                    block1.append(' the average value of ')
-                case _:
-                    block1.append(' the values of ')
+            elif condType == 'limit':
+                # get number of rows in relation -> numRows
+                numRows = Question.findNumRows(self, relation, attr_1)
+                lim = random.randrange(1, numRows, 1)
+                print("SELECT " + attr_1.name + " FROM " +
+                      relation.name + " LIMIT " + str(lim) + ';')
 
-            #attribute/s
-            block1[x] += attributes[x]
+            elif condType == 'where':
+                nullOrVal = random.choice(['null', 'val'])
+                # nullOrVal = 'null'
 
-            if len(attributes) > 1:
-                if x == len(attributes) - 2:
-                    block1[x] += ' and'
-                else:
-                    block1[x] += ','
+                attr_2 = self.setAttr(relation, aggOrCond, condType, 2)
+                # print("Attribute 2: " + attr_2.name) # testing (delete me)
 
-        return block1
+                # an attribute is not null
+                if nullOrVal == 'null' and attr_2.null == 'YES':
+                    '''Should I first check if attribute 2 actually contains any nulls? That feels unneccesary?'''
+                    print("SELECT " + attr_1.name + " FROM " +
+                          relation.name + " WHERE " + attr_2.name + " IS NOT NULL;")
 
 class EasyQuestion(Question):
 
-    def __init__(self, database, seed):
-        super().__init__(database, seed)
+            else:
+                print("Invalid condition")
 
 
 class MediumQuestion(Question):
@@ -200,9 +215,9 @@ class DifficultQuestion(Question):
 
 
 # testing (delete me)
-#newDB = Database.Database(db_name='sakila')
+newDB = Database.Database(db_name='sakila')
 # print(newDB.numRelations())
-#newQ = Question(newDB, 'seed')
+newQ = Question(newDB, 'seed')
 
-sql = [['',''],['city', 'country'], 'offices']
+sql = [['count'],['*'], 'offices']
 Question.englishQuestion(sql)
