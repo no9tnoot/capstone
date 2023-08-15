1027f3995d066816e841ffb0a965f6862e7ce5ad
emollyr <112810634+emollyr@users.noreply.github.com>
2023-08-15 12:03:56 +0200

    Comments added to UI class

--- UserInterface.py
+++ UserInterface.py
@@ -5,6 +5,7 @@
 import Session
 import Question
 
+""" Class to display information to the user and get input from the user """
 class UserInterface:
     
     def __init__(self):
@@ -12,19 +13,22 @@ class UserInterface:
         user = input("Please enter your student number:")
         self.session = Session.Session(user)
         choice = input("To attempt a question, enter q, to exit, enter x:")
+        
+        # While not choosing to exit
         while (choice != 'x'):
             if (choice=='q'):
                 self.askQuestion()
             else:
                 print("Invalid selection. Please try again.")
             choice=input("To attempt a question, enter q, to exit, enter x:")
+        #exit
         
     def askQuestion(self):
-        question = self.session.getQuestion()
-        # print the question
+        question = self.session.genQuestion()
+        # print the english question
         # take user input
-        # pass both to session marker
-        # return feedback
+        # pass both to session marker ( self.session.marker )
+        # return feedback (store it if we have a student object)
     
         
         
