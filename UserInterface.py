35946a78da76c9fb3495814d35b8535107963df8
emollyr <112810634+emollyr@users.noreply.github.com>
2023-08-16 20:47:39 +0200

    Changed UI entry to view question to blank, because q means quit :)

--- UserInterface.py
+++ UserInterface.py
@@ -12,15 +12,15 @@ class UserInterface():
         print("Hello!")
         user = input("Please enter your student number:")
         self.session = Session.Session(user)
-        choice = input("To attempt a question, enter q, to exit, enter x:")
+        choice = input("To attempt a question, press enter. To exit, enter x:")
         
         # While not choosing to exit
         while (choice != 'x'):
-            if (choice=='q'):
+            if (choice==''):
                 self.askQuestion()
             else:
                 print("Invalid selection. Please try again.")
-            choice=input("To attempt a question, enter q, to exit, enter x:")
+            choice=input("To attempt a question, press enter. To exit, enter x:")
         #exit
         
     def askQuestion(self):
