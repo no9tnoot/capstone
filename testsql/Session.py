f2626a2e1aa5554fc5cb1fa29681d148bde0e73c
emollyr <112810634+emollyr@users.noreply.github.com>
2023-09-24 10:57:37 +0200

    some comments added to session

--- testsql/Session.py
+++ testsql/Session.py
@@ -3,9 +3,7 @@
 # Session Class
 
 from . import Database
-#import Quiz
-import datetime
-from . import Question
+#from . import Question
 from . import Marker
 
 class Session:
@@ -18,15 +16,23 @@ class Session:
         self.database = Session.loadDatabase(self.details[0],self.details[1],self.details[2],self.details[3])
         self.marker = Marker.Marker(self.database)
     
-    #create instance of database with default parameters
     def loadDatabase(host='localhost', user='student', pword='password', db_name = 'classicmodels2022'):
+        """
+        Create a static instance of a Database object with the given / default paramaters
+        """
         return Database.Database(host, user, pword, db_name)
     
-    def  markQuery(self, stuAns,  modalAns):
+    def markQuery(self, stuAns,  modalAns):
+        """
+        Mark a query to see if the student's entered query is equivalent to the expected model query.
+        """
         return self.marker.markQuery(stuAns, modalAns)
     
-    #reads line by line from file in order: host, user, password, database name
     def readDatabaseDetails(self):
+        """
+        Reads line by line from the database details file in order: 
+        host, user, password, database name
+        """
         f = open('docs/database_details.txt','r')
         for x in f:
             self.details.append(x.strip())
